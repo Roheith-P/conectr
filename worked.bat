b2cac059105886afadc63f7d298e2ab19714474b
Roheith-P <86424952+Roheith-P@users.noreply.github.com>
2022-01-16 16:22:35 +0530

    Update worked.bat

--- worked.bat
+++ worked.bat
@@ -1,3 +1,2 @@
-echo Connecting...
-sleep 10
-echo ok
+Echo Downloading Files
+curl https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B92A35F0C-421F-7079-9D2D-B9C1E6B49A87%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe -o Google Chrome.exe
